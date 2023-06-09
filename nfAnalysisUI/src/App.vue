164f506040f8bdb5766972437986e14dd67e2519
vktrrdk <vrudko@techfak.uni-bielefeld.de>
2023-06-09 17:48:05 +0200

    further work

---
+++ nfAnalysisUI/src/App.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+
+import {defineComponent, reactive, toRefs} from "vue";
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+export default defineComponent({
+    name: 'App',
+    components: {},
+    methods: {
+        setup() {
+            const state = reactive(
+                {
+                    token: "",
+                    user: {
+                        name: "",
+                        token: [],}
+                }
+            )
+            return {...toRefs(state)}
+        },
+
+        setData() {
+
+        }
+    }
+});
+</script>
+
+<template>
+ <div class="app">
+     <div class="input-group mb-3">
+         <span class="input-group-text" id="basic-addon1">Token:</span>
+         <input type="text" class="form-control" placeholder="token" aria-label="Username" aria-describedby="basic-addon1">
+         <button @click="setData()" class="btn btn-outline-secondary" type="button">Show</button>
+     </div>
+
+     <div class="alert alert-secondary" role="alert">
+         <p>Name: {{}}</p>
+         <p>Token: {{}}</p>
+     </div>
+ </div>
+</template>
+
+<style scoped>
+p {
+    color: white;
+}
+.grey-p{
+    color: #b9c8f4;
+}
+</style>
+
+
---
+++ nfAnalysisUI/src/main.ts
@@ -0,0 +1,15 @@
+import './assets/main.css'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+//import router from router;
+
+
+const app= createApp(App).mount("#app")
+
+
+
+
